7a27433aa9c4c7b701725c650dbcbfc51524188d
ADITYA KUMAR <141934999+aditya-8072@users.noreply.github.com>
2024-02-16 03:25:56 +0530

    Add files via upload

---
+++ Guess_Number.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int secret_number;
+    int guess_number;
+    
+    int i,guess_limit=3;
+    secret_number=5;
+
+    for(i=1;i<=guess_limit;i++)
+    {
+        printf("Enter Guess Number : ",i);
+        scanf("%d",&guess_number);
+
+        if(guess_number==secret_number)
+        {
+            printf("You Won!");
+            printf("You have guessed in %d try ",i);
+            break;
+
+        }
+
+
+    }
+    if(guess_number!=secret_number)
+    printf("You Lost!");
+
+    return 0;
+}
